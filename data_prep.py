688334a63634b414ec11fa50e0dd9dd2e4467ed9
Jacob-Stahl <jacob.stahl@ttu,edu>
2020-04-28 00:19:49 -0500

    loaded dataset

--- data_prep.py
+++ data_prep.py
@@ -1,1 +1,8 @@
 import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(pd.read_csv("spam.csv"))
+df.drop(df.columns[[2,3,4]], axis=1, inplace=True)
+df.rename(columns={"v1": "label", "v2": "text"}, inplace = True)
+
+print(df)
